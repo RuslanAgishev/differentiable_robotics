6cabf5d4d23f2d88af7f8e875c30d27b4ade61d4
RuslanAgishev <agishrus@fel.cvut.cz>
2024-09-03 11:04:49 +0200

    learning terrain properties: stiffness, damping, friction

--- scripts/ronot_terrain_interaction.py
+++ scripts/ronot_terrain_interaction.py
@@ -8,9 +8,8 @@ plt.switch_backend('Qt5Agg')
 
 
 g = 9.81
-m = 1.0
 dt = 0.1
-T = 6.0
+T = 4.0
 d_max = 6.4
 grid_res = 0.1
 
@@ -24,7 +23,6 @@ def set_axes_equal(ax):
     Input
       ax: a matplotlib axis, e.g., as output from plt.gca().
     """
-
     x_limits = ax.get_xlim3d()
     y_limits = ax.get_ylim3d()
     z_limits = ax.get_zlim3d()
@@ -87,9 +85,12 @@ def interpolate_height(x_grid, y_grid, z_grid, x_query, y_query):
 
     return interpolated_z
 
-def force(x, v, m, x_grid, y_grid, z_grid):
+def force(x, xd, m, x_grid, y_grid, z_grid, k_stiffness=100.0, k_damping=5.0, k_friction=0.5):
+    """
+    Calculates the force acting on the point mass.
+    """
     # gravity force
-    F_g = torch.tensor([0.0, 0.0, -m * g])
+    F_grav = torch.tensor([0.0, 0.0, -m * g])
 
     # get height of terrain at current position
     x_i, y_i = int((x[0].item() + d_max) / grid_res), int((x[1].item() + d_max) / grid_res)
@@ -100,7 +101,8 @@ def force(x, v, m, x_grid, y_grid, z_grid):
 
     # if no contact with terrain, return zero force
     dh = x[2] - z
-    in_contact = dh <= 0
+    # in_contact = dh <= 0
+    in_contact = torch.sigmoid(-dh)
 
     # calculate normal vector of terrain at current position
     dz_dx = (z_grid[x_i + 1, y_i] - z_grid[x_i - 1, y_i]) / 2
@@ -108,12 +110,14 @@ def force(x, v, m, x_grid, y_grid, z_grid):
     n = torch.cross(torch.tensor([1.0, 0.0, dz_dx]), torch.tensor([0.0, 1.0, dz_dy]))
     n = n / torch.norm(n)
 
-    # calculate force: underdamped spring-damper system
-    k = 100.0
-    omega0 = torch.sqrt(torch.tensor(k / m))
-    d = 0.5 * omega0
-    F_t = -(k * dh + d * v) * n * in_contact
-    F = F_g + F_t
+    # calculate terrain reaction force: underdamped spring-damper system: https://beltoforion.de/en/harmonic_oscillator/
+    F_spring = -(k_stiffness * dh + k_damping * xd) * n * in_contact
+
+    # friction force
+    F_friction = -k_friction * xd * in_contact
+
+    # total force
+    F = F_grav + F_spring + F_friction
 
     return F
 
@@ -136,12 +140,15 @@ def step_runge_kutta(x, xd, xdd, dt):
     return x, xd
 
 
-def dphysics(x, xd, x_grid, y_grid, z_grid):
+def dphysics(x, xd, x_grid, y_grid, z_grid, m=1.0, k_stiffness=100.0, k_damping=5.0, k_friction=0.5):
+    """
+    Simulates the motion of a point mass under the influence of gravity and terrain
+    """
     xs = []
     vs = []
     forces = []
     for i in range(int(T / dt)):
-        F = force(x, xd, m, x_grid, y_grid, z_grid)
+        F = force(x, xd, m, x_grid, y_grid, z_grid, k_stiffness, k_damping, k_friction)
         xdd = F / m
         # x, xd = step(x, xd, xdd, dt)
         x, xd = step_runge_kutta(x, xd, xdd, dt)
@@ -170,11 +177,11 @@ def forward():
     y_grid = torch.arange(-d_max, d_max, grid_res)
     x_grid, y_grid = torch.meshgrid(x_grid, y_grid)
     # z_grid = torch.zeros(x_grid.shape)
-    z_grid = torch.sin(x_grid) + torch.cos(y_grid)
-    # z_grid = torch.exp(-x_grid**2 / 10) * torch.exp(-y_grid**2 / 10)
+    # z_grid = torch.sin(x_grid) + torch.cos(y_grid)
+    z_grid = torch.exp(-x_grid**2 / 10) * torch.exp(-y_grid**2 / 10)
 
     # simulate point mass motion
-    xs, vs, forces = dphysics(x0, v0, x_grid, y_grid, z_grid)
+    xs, vs, forces = dphysics(x0, v0, x_grid, y_grid, z_grid, m=1)
 
     # plot results
     fig = plt.figure(figsize=(10, 10))
@@ -193,14 +200,17 @@ def forward():
         if i % 1 == 0:
             # plot point mass
             ax.scatter(x[0].item(), x[1].item(), x[2].item(), c='r')
-            # plot total force
-            ax.quiver(x[0].item(), x[1].item(), x[2].item(), F[0].item(), F[1].item(), F[2].item(), color='m')
+            # # plot total force
+            # ax.quiver(x[0].item(), x[1].item(), x[2].item(),
+            #           F[0].item() / g, F[1].item() / g, F[2].item() / g, color='m')
+            # # plot velocity
+            # ax.quiver(x[0].item(), x[1].item(), x[2].item(), v[0].item(), v[1].item(), v[2].item(), color='g')
             plt.pause(0.01)
 
     plt.show()
 
 
-def optimization():
+def learn_height():
     """
     optimize point mass trajectory
     """
@@ -220,17 +230,27 @@ def optimization():
     # optimize heightmap to fit the trajectory
     z_grid = torch.zeros(x_grid.shape, requires_grad=True)
     optimizer = torch.optim.Adam([z_grid], lr=0.01)
-    for i in tqdm(range(200)):
+    n_iters = 1_000
+    loss_history = []
+    for i in tqdm(range(n_iters)):
         optimizer.zero_grad()
         xs, vs, forces = dphysics(x0, v0, x_grid, y_grid, z_grid)
-        loss = torch.sum((xs - xs_gt) ** 2)
+        loss_x = torch.sum((xs - xs_gt) ** 2)
+        # loss_v = torch.sum((vs - vs_gt) ** 2)
+        # loss = loss_x + loss_v
+        loss = loss_x
         loss.backward()
         optimizer.step()
-        print('Loss:', loss.item())
+        loss_history.append(loss.item())
+
+        if i % 10 == 0:
+            # print('Loss X:', loss_x.item())
+            # print('Loss V:', loss_v.item())
+            print('Loss:', loss.item())
 
     # plot results
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
+    fig = plt.figure(figsize=(20, 10))
+    ax = fig.add_subplot(121, projection='3d')
     # ax.plot_surface(y_grid.numpy(), x_grid.numpy(), z_grid_gt.numpy(), alpha=0.5, cmap='viridis')
     ax.plot_surface(y_grid.numpy(), x_grid.numpy(), z_grid.detach().numpy(), alpha=0.5, cmap='viridis')
     # plot trajectories
@@ -240,11 +260,86 @@ def optimization():
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
     set_axes_equal(ax)
+
+    ax = fig.add_subplot(122)
+    ax.plot(loss_history)
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('Loss')
+
     plt.show()
 
+
+def learn_terrain_properties():
+    """
+    optimize terrain properties to fit the trajectory
+    """
+    # initial position and velocity
+    x0 = torch.tensor([-4.0, 0.0, 5.0])
+    v0 = torch.tensor([1.0, 0.2, 0.0])
+
+    # heightmap defining the terrain
+    x_grid = torch.arange(-d_max, d_max, grid_res)
+    y_grid = torch.arange(-d_max, d_max, grid_res)
+    x_grid, y_grid = torch.meshgrid(x_grid, y_grid)
+    z_grid = torch.sin(x_grid) + torch.cos(y_grid)
+
+    # simulate point mass motion
+    xs_gt, vs_gt, forces_gt = dphysics(x0, v0, x_grid, y_grid, z_grid)
+
+    # optimize terrain and robot properties to fit the trajectory
+    k_stiffness = torch.tensor(150.0, requires_grad=True)  # GT: 100.0
+    m = torch.tensor(1.0, requires_grad=False)  # GT: 1.0
+    omega0 = torch.sqrt(k_stiffness / m)
+    k_damping = torch.tensor(0.3 * omega0, requires_grad=True)  # GT: 0.5 * omega0
+    k_friction = torch.tensor(0.4, requires_grad=True)  # GT: 0.5
+    optimizer = torch.optim.Adam([
+        {'params': k_stiffness, 'lr': 0.1},
+        {'params': k_damping, 'lr': 0.01},
+        {'params': k_friction, 'lr': 0.01},
+        # {'params': m, 'lr': 0.1}
+    ])
+    n_iters = 1_000
+    loss_history = []
+    for i in tqdm(range(n_iters)):
+        optimizer.zero_grad()
+        xs, vs, forces = dphysics(x0, v0, x_grid, y_grid, z_grid, m, k_stiffness, k_damping, k_friction)
+        loss = torch.sum((xs - xs_gt) ** 2) + torch.sum((vs - vs_gt) ** 2)
+        loss.backward()
+        optimizer.step()
+        loss_history.append(loss.item())
+
+        if i % 10 == 0:
+            print('Loss:', loss.item())
+            print('k_stiffness:', k_stiffness.item())
+            print('k_damping:', k_damping.item())
+            print('k_friction:', k_friction.item())
+            print('m:', m.item())
+
+        if i == 0 or i == n_iters-1:
+            # plot results
+            fig = plt.figure(figsize=(20, 10))
+            ax = fig.add_subplot(121, projection='3d')
+            ax.plot_surface(y_grid.numpy(), x_grid.numpy(), z_grid.numpy(), alpha=0.5, cmap='viridis')
+            # plot trajectories
+            ax.plot(xs_gt[:, 0].detach().numpy(), xs_gt[:, 1].detach().numpy(), xs_gt[:, 2].detach().numpy(), c='r')
+            ax.plot(xs[:, 0].detach().numpy(), xs[:, 1].detach().numpy(), xs[:, 2].detach().numpy(), c='b')
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
+            set_axes_equal(ax)
+
+            ax = fig.add_subplot(122)
+            ax.plot(loss_history)
+            ax.set_xlabel('Iteration')
+            ax.set_ylabel('Loss')
+
+            plt.show()
+
+
 def main():
-    forward()
-    # optimization()
+    # forward()
+    # learn_height()
+    learn_terrain_properties()
 
 
 if __name__ == '__main__':
