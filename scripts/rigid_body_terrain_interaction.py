rigid body motion: fixed bug in normal force computation: using normal component of the penetraiting velocity + refactoring: dphysics simulation function

scripts/rigid_body_terrain_interaction.py
@@ -1,13 +1,13 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 # set matplotlib backend to show plots in a separate window
 plt.switch_backend('Qt5Agg')
 
 
 g = 9.81
-dt = 0.01
-T = 10.0
 d_max = 6.4
 grid_res = 0.1
 
@@ -194,8 +194,8 @@ def rigid_body_params():
     # x_points = x_points * torch.tensor([0.8, 0.5, 0.2])
 
     import open3d as o3d
-    robot = 'tradr'
-    mesh_file = f'/home/ruslan/workspaces/traversability_ws/src/monoforce/monoforce/data/meshes/{robot}.obj'
+    robot = 'husky'
+    mesh_file = f'../data/meshes/{robot}.obj'
     mesh = o3d.io.read_triangle_mesh(mesh_file)
     n_points = 20
     x_points = np.asarray(mesh.sample_points_uniformly(n_points).points)
@@ -253,18 +253,17 @@ def forward(x, xd, R, omega, x_points, xd_points,
 
     # compute surface normals and tangents at the contact points
     n, _, _ = surface_normals_and_tangents(x_grid, y_grid, z_grid, x_points[:, 0], x_points[:, 1])
-    n = n * in_contact
 
     # reaction at the contact points as spring-damper forces
-    F_spring = -(k_stiffness * dh_points + k_damping * xd_points) * n * in_contact
-    # only allow forces in the z direction
-    F_spring = F_spring * torch.sign(F_spring @ torch.tensor([[0.0, 0.0, 1.0]]).T)
+    xd_points_n = (xd_points * n).sum(dim=-1, keepdims=True)  # normal velocity
+    F_spring = -(k_stiffness * dh_points + k_damping * xd_points_n) * n * in_contact
     # # avoid too large forces
     # F_spring = torch.clamp(F_spring, -10*m * g, 10*m * g)
 
     # friction forces: https://en.wikipedia.org/wiki/Friction
     N = torch.norm(F_spring, dim=-1, keepdim=True)
-    tau = normailized(xd_points - (xd_points * n).sum(dim=-1, keepdims=True) * n)
+    xd_points_tau = xd_points - xd_points_n * n
+    tau = normailized(xd_points_tau)
     F_friction = -k_friction * N * tau * in_contact
 
     # thrust forces: left and right
@@ -278,10 +277,10 @@ def forward(x, xd, R, omega, x_points, xd_points,
     torque_thrust = torque_left + torque_right
 
     # rigid body rotation
-    torque = torch.sum(torch.cross(x_points - x, F_spring + F_friction), dim=0) + torque_thrust
-    omega_d = I_inv @ torque
-    omega_skew = skew_symmetric(omega)
-    dR = omega_skew @ R
+    torque = torch.sum(torch.cross(x_points - x, F_spring + F_friction), dim=0) + torque_thrust  # M = sum(r_i x F_i)
+    omega_d = I_inv @ torque  # omega_d = I^(-1) M
+    omega_skew = skew_symmetric(omega)  # omega_skew = [omega]_x
+    dR = omega_skew @ R  # dR = [omega]_x R
 
     # motion of the cog
     F_grav = torch.tensor([0.0, 0.0, -m * g])
@@ -303,12 +302,52 @@ def update_states(x, xd, xdd, R, dR, omega, omega_d, x_points, xd_points, dt):
 
     return x, xd, R, omega, x_points
 
+def dphysics(state, xd_points,
+             x_grid, y_grid, z_grid,
+             m, I, mask_left, mask_right, controls,
+             k_stiffness=1000., k_damping=None, k_friction=0.02,
+             T=10.0, dt=0.01):
+    # state: x, xd, R, omega, x_points
+    x, xd, R, omega, x_points = state
+
+    I_inv = torch.inverse(I)
+    if k_damping is None:
+        k_damping = np.sqrt(4 * m * k_stiffness)  # critically damping
+
+    # dynamics of the rigid body
+    states = []
+    forces = []
+    for i in range(int(T / dt)):
+        # control inputs
+        u_left, u_right = controls[i]  # thrust forces, Newtons or kg*m/s^2
+        # forward kinematics
+        (xd, xdd, dR, omega_d, xd_points,
+         F_spring, F_friction, F_thrust_left, F_thrust_right) = forward(x, xd, R, omega, x_points, xd_points,
+                                                                        x_grid, y_grid, z_grid,
+                                                                        m, I_inv, mask_left, mask_right, u_left,
+                                                                        u_right,
+                                                                        k_stiffness, k_damping, k_friction)
+        # update states: integration steps
+        x, xd, R, omega, x_points = update_states(x, xd, xdd, R, dR, omega, omega_d, x_points, xd_points, dt)
+
+        # store states
+        states.append((x, xd, R, omega, x_points))
+
+        # store forces
+        forces.append((F_spring, F_friction, F_thrust_left, F_thrust_right))
+
+    return states, forces
+
 def motion():
-    # rigid body points
+    # simulation parameters
+    dt = 0.01
+    T = 5.0
+
+    # rigid body parameters
     x_points, m, I, mask_left, mask_right = rigid_body_params()
 
     # initial state
-    x = torch.tensor([1.0, 0.0, 1.0])
+    x = torch.tensor([1.0, 0.0, 3.0])
     xd = torch.tensor([0.0, 0.0, 0.0])
     R = torch.eye(3)
     omega = torch.tensor([0.0, 0.0, 0.0])
@@ -318,83 +357,74 @@ def motion():
     # heightmap defining the terrain
     x_grid, y_grid, z_grid = heightmap(d_max, grid_res)
 
-    # plot results
+    # control inputs
+    controls = torch.tensor([[10.0, 10.0]] * int(T / dt))
+
+    # initial state
+    state0 = (x, xd, R, omega, x_points)
+
+    # simulate the rigid body dynamics
+    states, forces = dphysics(state0, xd_points,
+                              x_grid, y_grid, z_grid,
+                              m, I, mask_left, mask_right,
+                              controls,
+                              T=T, dt=dt)
+    # visualize
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
+    with torch.no_grad():
+        for i, (state, F) in tqdm(enumerate(zip(states[::10], forces[::10])), total=len(states[::10])):
+            x, xd, R, omega, x_points = state
+            F_spring, F_friction, F_thrust_left, F_thrust_right = F
 
-    # motion of the rigid body
-    xs = []
-    I_inv = torch.inverse(I)
-    k_stiffness = 1_000.0
-    k_damping = np.sqrt(4 * m * k_stiffness)  # critical damping
-    k_friction = 0.01
-    for i in range(int(T / dt)):
-        # control inputs
-        u_left, u_right = 10.0, 10.0  # thrust forces, Newtons or kg*m/s^2
-        # forward dynamics
-        (xd, xdd, dR, omega_d, xd_points,
-         F_spring, F_friction, F_thrust_left, F_thrust_right) = forward(x, xd, R, omega, x_points, xd_points,
-                                                                        x_grid, y_grid, z_grid,
-                                                                        m, I_inv, mask_left, mask_right, u_left, u_right,
-                                                                        k_stiffness, k_damping, k_friction)
-        # update states: integration steps
-        x, xd, R, omega, x_points = update_states(x, xd, xdd, R, dR, omega, omega_d, x_points, xd_points, dt)
+            plt.cla()
+            ax.set_xlabel('X')
+            ax.set_ylabel('Y')
+            ax.set_zlabel('Z')
 
-        # store trajectory
-        xs.append(x)
+            # plot rigid body points and cog
+            ax.scatter(x_points[:, 0].numpy(), x_points[:, 1].numpy(), x_points[:, 2].numpy(), c='k')
+            ax.scatter(x[0].item(), x[1].item(), x[2].item(), c='r')
 
-        # plot results
-        if i % 10 == 0:
-            with torch.no_grad():
-                plt.cla()
-                ax.set_xlabel('X')
-                ax.set_ylabel('Y')
-                ax.set_zlabel('Z')
-                # time in seconds in the title
-                ax.set_title(f'Time: {i * dt:.2f} s')
+            # plot rigid body frame
+            x_axis = R @ torch.tensor([1.0, 0.0, 0.0])
+            y_axis = R @ torch.tensor([0.0, 1.0, 0.0])
+            z_axis = R @ torch.tensor([0.0, 0.0, 1.0])
+            ax.quiver(x[0], x[1], x[2], x_axis[0], x_axis[1], x_axis[2], color='r')
+            ax.quiver(x[0], x[1], x[2], y_axis[0], y_axis[1], y_axis[2], color='g')
+            ax.quiver(x[0], x[1], x[2], z_axis[0], z_axis[1], z_axis[2], color='b')
 
-                # plot rigid body points and cog
-                ax.scatter(x_points[:, 0].numpy(), x_points[:, 1].numpy(), x_points[:, 2].numpy(), c='k')
-                ax.scatter(x[0].item(), x[1].item(), x[2].item(), c='r')
+            # plot trajectory
+            xs_tensor = torch.stack([s[0] for s in states])
+            ax.plot(xs_tensor[:, 0].numpy(), xs_tensor[:, 1].numpy(), xs_tensor[:, 2].numpy(), c='b')
 
-                # plot rigid body frame
-                x_axis = R @ torch.tensor([1.0, 0.0, 0.0])
-                y_axis = R @ torch.tensor([0.0, 1.0, 0.0])
-                z_axis = R @ torch.tensor([0.0, 0.0, 1.0])
-                ax.quiver(x[0], x[1], x[2], x_axis[0], x_axis[1], x_axis[2], color='r')
-                ax.quiver(x[0], x[1], x[2], y_axis[0], y_axis[1], y_axis[2], color='g')
-                ax.quiver(x[0], x[1], x[2], z_axis[0], z_axis[1], z_axis[2], color='b')
+            # plot cog velocity
+            ax.quiver(x[0], x[1], x[2], xd[0], xd[1], xd[2], color='k')
 
-                # plot trajectory
-                xs_tensor = torch.stack(xs)
-                ax.plot(xs_tensor[:, 0].numpy(), xs_tensor[:, 1].numpy(), xs_tensor[:, 2].numpy(), c='b')
+            # plot terrain
+            ax.plot_surface(x_grid.numpy(), y_grid.numpy(), z_grid.numpy(), alpha=0.5, cmap='terrain')
 
-                # plot cog velocity
-                ax.quiver(x[0], x[1], x[2], xd[0], xd[1], xd[2], color='k')
+            # # plot normal forces
+            # ax.quiver(x_points[:, 0], x_points[:, 1], x_points[:, 2], F_spring[:, 0], F_spring[:, 1], F_spring[:, 2], color='b')
+            #
+            # # plot friction forces
+            # ax.quiver(x_points[:, 0], x_points[:, 1], x_points[:, 2], F_friction[:, 0], F_friction[:, 1], F_friction[:, 2], color='g')
+            #
+            # # plot thrust forces
+            # ax.quiver(x_points[mask_left].mean(dim=0)[0], x_points[mask_left].mean(dim=0)[1], x_points[mask_left].mean(dim=0)[2],
+            #           F_thrust_left[0], F_thrust_left[1], F_thrust_left[2], color='r')
+            # ax.quiver(x_points[mask_right].mean(dim=0)[0], x_points[mask_right].mean(dim=0)[1], x_points[mask_right].mean(dim=0)[2],
+            #           F_thrust_right[0], F_thrust_right[1], F_thrust_right[2], color='r')
 
-                # plot terrain
-                ax.plot_surface(x_grid.numpy(), y_grid.numpy(), z_grid.numpy(), alpha=0.5, cmap='terrain')
+            set_axes_equal(ax)
+            plt.pause(0.01)
 
-                # # plot normal forces
-                # ax.quiver(x_points[:, 0], x_points[:, 1], x_points[:, 2], F_spring[:, 0], F_spring[:, 1], F_spring[:, 2], color='b')
-
-                # # plot friction forces
-                # ax.quiver(x_points[:, 0], x_points[:, 1], x_points[:, 2], F_friction[:, 0], F_friction[:, 1], F_friction[:, 2], color='g')
-
-                # plot thrust forces
-                ax.quiver(x_points[mask_left].mean(dim=0)[0], x_points[mask_left].mean(dim=0)[1], x_points[mask_left].mean(dim=0)[2],
-                          F_thrust_left[0], F_thrust_left[1], F_thrust_left[2], color='r')
-                ax.quiver(x_points[mask_right].mean(dim=0)[0], x_points[mask_right].mean(dim=0)[1], x_points[mask_right].mean(dim=0)[2],
-                          F_thrust_right[0], F_thrust_right[1], F_thrust_right[2], color='r')
-
-                set_axes_equal(ax)
-                plt.pause(0.01)
-
-    plt.show()
+        plt.show()
 
 
 def main():
     motion()
+    # optimization()
 
 
 if __name__ == '__main__':